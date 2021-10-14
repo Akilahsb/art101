7c99c1b9bc55640061accedcec457ab599532903
Akilahsb <akilahbussey@gmail.com>
2021-10-13 20:28:18 -0700

    added js comments, added lab 4 page, fixed site.css

---
+++ lab4/js/lab.js
@@ -0,0 +1,19 @@
+<pre>
+<code>
+
+// This is a checkers game.
+// what you got to do is win the game.
+// @link url
+// @file  This file defines the Art1o1-01 class.
+//  @author AkilahBussey
+// @since 10.13.2021
+
+
+//Display board before game starts.
+//Evaluate user’s move after each turn.
+//Evaluate possible moves.
+//Choose best computer move.
+//Repeat until winner is determined.
+
+</code>
+</pre>
